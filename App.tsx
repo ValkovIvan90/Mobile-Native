8d69ff936a1e8e2b226c9a0a6f81d97a39fff41f
Ivan Valkov <johnsson738@gmail.com>
2023-01-22 19:04:21 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo cumque provident corporis! Consequuntur ex at unde nesciunt laborum earum perferendis exercitationem blanditiis ullam quae commodi deleniti quia soluta perspiciatis facere molestiae vitae placeat, aspernatur ipsum quaerat illum iste! Est, harum expedita culpa laudantium perspiciatis ipsum aperiam eum labore provident et quasi inventore officia non eius doloremque ad facere eveniet. Laudantium corporis perspiciatis, culpa, magni esse neque velit asperiores nihil eveniet vero officia inventore quis possimus impedit explicabo veniam commodi! Ullam voluptatum, vitae, dolores nobis ipsum consequuntur minima, soluta qui laudantium quod quos in neque fuga veniam. Quisquam voluptas ut optio.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff2d',
     alignItems: 'center',
     justifyContent: 'center',
   },
